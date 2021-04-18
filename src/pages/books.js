828b92739814deb7d41e00e9e2b0dacb5e1c24e3
corwic <v.corwic@gmail.com>
2021-04-18 19:05:57 +0200

    mongoDB library in use to GET

--- src/pages/books.js
+++ src/pages/books.js
@@ -6,12 +6,12 @@ import List from '../components/List'
 
 import { useDispatch } from 'react-redux'
 import booksSlice, { fetchBooks } from '../redux/booksSlice'
+import { connectToDatabase } from '../utils/mongodb'
 
 
-
-export default function Books(/*{ booksData }*/) {
+export default function Books( { books } /*{ booksData }*/ ) {
   const dispatch = useDispatch()
-  const { books } = booksSlice()
+  //const { books } = booksSlice()
 
   useEffect(() => {
     dispatch( fetchBooks )
@@ -28,14 +28,39 @@ export default function Books(/*{ booksData }*/) {
     </Layout>
   )
 }
+
+export async function getStaticProps() {
+  const { db } = await connectToDatabase();
+  const books = await db
+    .collection("books")
+    .find({})
+    .limit(20)
+    .toArray();
+  return {
+    props: {
+      books: JSON.parse(JSON.stringify(books)),
+    },
+  };
+}
+
+/*export async function getServerSideProps(context) {
+  const { client } = await connectToDatabase()
+
+  const isConnected = await client.isConnected()
+
+  return {
+    props: { isConnected },
+  }
+}*/
+
+
+
 /*
 export async function getStaticProps() {
   // Call an external API endpoint to get posts.
   // You can use any data fetching library
   const res = await fetch( process.env.NEXT_PUBLIC_API + 'books' )
   const booksData = await res.json()
-
-
 
   // By returning { props: { posts } }, the Blog component
   // will receive `posts` as a prop at build time
