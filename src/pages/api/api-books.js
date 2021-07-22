f99ac82110945a2ad7cdcfd9bb629fbe80934ce9
corwic <v.corwic@gmail.com>
2021-07-22 18:30:15 +0200

    try to connect to goodreads

--- src/pages/api/api-books.js
+++ src/pages/api/api-books.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-const books = require("../../data/books.json")
+const books = require("../../books/books.json")
 
 export default (req, res) => {
   res.status(200).json(books)
